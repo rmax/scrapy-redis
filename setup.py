cc55fadf772cec6bdfa852a02cdca95d9ea67862
Rolando Espinoza La fuente <darkrho@gmail.com>
2013-02-17 10:43:48 -0400

    added dependencies and classifiers, bump 0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+import os
 from distutils.core import setup
 
+
+LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
 setup(name='scrapy-redis',
-      version='0.1',
+      version='0.2',
       description='Redis-based components for Scrapy',
+      long_description=LONG_DESC,
       author='Rolando Espinoza La fuente',
       author_email='darkrho@gmail.com',
       url='http://github.com/darkrho/scrapy-redis',
       packages=['scrapy_redis'],
       license='BSD',
-      #install_requires=['Scrapy>=0.13'],
+      install_requires=['Scrapy>=0.14', 'redis>=2.4'],
+      classifiers=[
+          'Programming Language :: Python',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+      ],
      )
