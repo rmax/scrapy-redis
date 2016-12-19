4b142fac73558760691156f70af701f693833d19
Rolando Espinoza <rndmax84@gmail.com>
2016-12-19 01:48:19 -0400

    BUG Fix comma typo in defaults values.

--- src/scrapy_redis/defaults.py
+++ src/scrapy_redis/defaults.py
@@ -18,8 +18,8 @@ REDIS_PARAMS = {
 
 SCHEDULER_QUEUE_KEY = '%(spider)s:requests'
 SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'
-SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter',
-SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter',
+SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'
+SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
 
 START_URLS_KEY = '%(name)s:start_urls'
 START_URLS_AS_SET = False
