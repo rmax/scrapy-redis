bf331f1e08d0fd89c89f599a17cadf788afb55f6
LuckyPigeon <lucky90322@gmail.com>
2022-02-27 05:06:16 +0800

    Update tutorial for json data support

--- src/scrapy_redis/spiders.py
+++ src/scrapy_redis/spiders.py
@@ -150,7 +150,7 @@ class RedisMixin(object):
         {"url": "https://exaple.com", "meta": {'job-id':'123xsd', 'start-date':'dd/mm/yy'}, "url_cookie_key":"fertxsas" }
 
         this data can be accessed from 'scrapy.spider' through response.
-        'response.url', 'response.meta', 'response.request.cookies'
+        'request.url', 'request.meta', 'request.cookies'
 
         Parameters
         ----------
