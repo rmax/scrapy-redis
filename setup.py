74ab18ce93358ba42052207a9d613f9a8cecf6e6
LuckyPigeon <lucky90322@gmail.com>
2022-03-30 06:01:39 +0800

    update python version

--- setup.py
+++ setup.py
@@ -2,83 +2,54 @@
 # -*- coding: utf-8 -*-
 import io
 from pkgutil import walk_packages
-from os.path import dirname, join
-from pkg_resources import parse_version
-from setuptools import setup, find_packages, __version__ as setuptools_version
+from setuptools import setup
 
 
-with open(join(dirname(__file__), 'VERSION'), 'rb') as f:
-    version = f.read().decode('ascii').strip()
+def find_packages(path):
+    # This method returns packages and subpackages as well.
+    return [name for _, name, is_pkg in walk_packages([path]) if is_pkg]
 
-def has_environment_marker_platform_impl_support():
-    """Code extracted from 'pytest/setup.py'
-    https://github.com/pytest-dev/pytest/blob/7538680c/setup.py#L31
-    The first known release to support environment marker with range operators
-    it is 18.5, see:
-    https://setuptools.readthedocs.io/en/latest/history.html#id235
-    """
-    return parse_version(setuptools_version) >= parse_version('18.5')
-    
-install_requires = [
-    'Scrapy>=1.0',
-    'redis>=3.0',
-    'six>=1.5.2'
-]
-tests_require = [
-    'coverage',
-    'flake8',
-    'mock',
-    'pytest',
-    'tox'
-]
-extras_require = {}
-cpython_dependencies = [
-    'lxml>=3.5.0',
-    'PyDispatcher>=2.0.5',
-]
 
-if has_environment_marker_platform_impl_support():
-    extras_require[':platform_python_implementation == "CPython"'] = cpython_dependencies
-    extras_require[':platform_python_implementation == "PyPy"'] = [
-        # Earlier lxml versions are affected by
-        # https://foss.heptapod.net/pypy/pypy/-/issues/2498,
-        # which was fixed in Cython 0.26, released on 2017-06-19, and used to
-        # generate the C headers of lxml release tarballs published since then, the
-        # first of which was:
-        'lxml>=4.0.0',
-        'PyPyDispatcher>=2.1.0',
-    ]
-else:
-    install_requires.extend(cpython_dependencies)
+def read_file(filename):
+    with io.open(filename) as fp:
+        return fp.read().strip()
+
+
+def read_rst(filename):
+    # Ignore unsupported directives by pypi.
+    content = read_file(filename)
+    return ''.join(line for line in io.StringIO(content)
+                   if not line.startswith('.. comment::'))
+
+
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
 
 setup(
     name='scrapy-redis',
-    version=version,
+    version=read_file('VERSION'),
     description="Redis-based components for Scrapy.",
-    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    long_description=read_rst('README.rst') + '\n\n' + read_rst('HISTORY.rst'),
     author="Rolando Espinoza",
     author_email='rolando@rmax.io',
     url='https://github.com/rolando/scrapy-redis',
-    packages=find_packages(exclude=('tests', 'tests.*')),
+    packages=list(find_packages('src')),
+    package_dir={'': 'src'},
+    install_requires=read_requirements('requirements.txt'),
     include_package_data=True,
-    zip_safe=False,
     license="MIT",
     keywords='scrapy-redis',
     classifiers=[
-        'Framework :: Scrapy-Redis',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-    python_requires='>=3.6',
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require=extras_require,
 )
