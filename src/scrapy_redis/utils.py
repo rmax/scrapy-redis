4fe36e19258fd21ff01f9444b96f549254df887b
Jeremy Chou <lucky90322@gmail.com>
2022-04-09 15:17:49 +0800

    Dev tox matrix (#244)
    * support scarpy 1.x
    
    * add python-scrapy tox matrix
    
    * fix flake8 style
    
    * add python-scrapy-redis matrix

--- src/scrapy_redis/utils.py
+++ src/scrapy_redis/utils.py
@@ -15,11 +15,13 @@ class TextColor:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def bytes_to_str(s, encoding='utf-8'):
     """Returns a str if a bytes object is given."""
     if six.PY3 and isinstance(s, bytes):
         return s.decode(encoding)
     return s
+
 
 def is_dict(string_content):
     try:
